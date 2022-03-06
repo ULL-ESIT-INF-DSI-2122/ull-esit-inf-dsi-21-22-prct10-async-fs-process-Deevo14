43a5802e2cd2c6e7caf42c3f4706217890db551b
Deevo14 <alu0101052168@ull.edu.es>
2022-03-06 17:37:08 +0000

    Entrar

---
+++ src/ejercicio-1.ts
@@ -0,0 +1,21 @@
+function productTable(N:number):(number[][]|string){
+
+    let result:number[][] = [];
+
+    if(N>=1){
+        for(let i:number = 0; i < N; i++){
+            result[i] = [];
+            for(let j:number = 0; j < N; j++){
+                result[i][j] = (i + 1) * (j + 1);
+                    
+            }
+        }
+
+        return result;
+    }
+
+    else{
+        return 'El número introducido tiene que ser mayor o igual que 1';
+    }
+}
+console.log(`${productTable(5)}`);
