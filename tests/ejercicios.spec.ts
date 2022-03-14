836d906e6dd2d51fdbc5afc759167625f4f20f57
Deevo14 <alu0101052168@ull.edu.es>
2022-03-14 01:21:21 +0000

    Estructura del Repositorio

--- tests/ejercicios.spec.ts
+++ tests/ejercicios.spec.ts
@@ -1,18 +1,3 @@
 /* eslint-disable max-len */
 import 'mocha';
 import {expect} from 'chai';
-import {productTable} from '../src/ejercicio-1';
-
-
-describe('productTable tests', () => {
-  it('productTable(2) returns [[1, 2], [2, 4]]', () => {
-    expect(productTable(2)).to.eql([[1, 2], [2, 4]]);
-  });
-  it('productTable(3) returns [[1, 2, 3], [2, 4, 6], [3, 6, 9]]', () => {
-    expect(productTable(3)).to.eql([[1, 2, 3], [2, 4, 6], [3, 6, 9]]);
-  });
-
-  it('productTable(4) returns [[1, 2, 3, 4], [2, 4, 6, 8], [3, 6, 9, 12], [4, 8, 12, 16]]', () => {
-    expect(productTable(4)).to.eql([[1, 2, 3, 4], [2, 4, 6, 8], [3, 6, 9, 12], [4, 8, 12, 16]]);
-  });
-});
