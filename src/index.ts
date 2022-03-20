7c0a369d5d0e627ee9d6c071bdb2cd905014aed6
Deevo14 <alu0101052168@ull.edu.es>
2022-03-20 23:25:29 +0000

    PEstructura del Repo

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * asd
+ * @param fn sda
+ * @param sn asd
+ * @returns asd
+ */
+export function add(fn:number, sn:number){
+    if (true) {
+        return fn + sn;
+    }
+    return -1;
+}
--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,26 +1,10 @@
 /* eslint-disable max-len */
 import 'mocha';
 import {expect} from 'chai';
-import {Combat} from '../src/ejercicio-1/combat';
-import {Pokedex} from '../src/ejercicio-1/pokedex';
-import {Pokemon} from '../src/ejercicio-1/pokemon';
+import {add} from '../src/index';
 
-describe('Comprobaciones Combat', () => {
-    const Venusaur = new Pokemon('Venusaur', 100, 2, 'plant', 82, 83, 80, 82);
-    const Charizard = new Pokemon('Charizard', 90, 1.7, 'fire', 84, 78, 100, 78);
-    const Pokedex1 = new Pokedex([]);
-    Pokedex1.añadirPokemon(Venusaur);
-    Pokedex1.añadirPokemon(Charizard);
-    const FirstCombat = new Combat(Charizard, Venusaur);
-    const testCombat = new Combat(Charizard, Venusaur);
-
-    it('Test Effectivity', () => {
-        expect(testCombat.effectivity('fire', 'water')).to.be.equal(0.5);
-        expect(testCombat.effectivity('electric', 'plant')).to.be.equal(1);
-        expect(testCombat.effectivity('fire', 'plant')).to.be.equal(2);
-    });
-
-    it('Test Combat', () => {
-        expect(FirstCombat.start()).to.be.equal('Charizard');
+describe('add tests', () => {
+    it('add(1,8) returns 9', () => {
+        expect(add(1, 8)).to.be.equal(9);
     });
 });
